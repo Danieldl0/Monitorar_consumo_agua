12e4b0ccc165a27be2ba69b8c88119c0b890f625
danieldl0 <daniel.d.oliveira20@gmail.com>
2023-10-15 20:06:59 -0300

    fix inicial page

--- monitora_consumo_agua/urls.py
+++ monitora_consumo_agua/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('consumo/', ConsumoView.as_view(), name='consumo_view'),
+    path('', ConsumoView.as_view(), name='consumo_view'),
     path('cadastro/teste/', CadastrarConsumoView.as_view(), name='cadastro_consumo'),
     path('cadastro/sensor/', CadastrarSensorView.as_view(), name='cadastro_sensor'),
     path('deletar/sensor/<int:id>/', DeletarSensorView.as_view(), name='deletar_sensor'),
